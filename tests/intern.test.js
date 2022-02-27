2f55b5090a6c00b309d472a2d872f162c2472a28
Unknown <danila.vpopov@gmail.com>
2022-02-27 09:58:25 -0800

    intern test fixed v2

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -2,12 +2,12 @@ const Intern = require("../lib/intern");
 
 test("education is set through the constructor", () => {
   const test = "Kindergarden";
-  const expected = new Intern("Any", 123, `Education: ${test}`);
-  expect(expected.school).toBe(test);
+  const expected = new Intern("Any", 123, test);
+  expect(expected.school).toBe(`Education: ${test}`);
 });
 
 test("school function outputs education", () => {
   const test = "Kindergarden";
-  const expected = new Intern("Any", 123, `Education: ${test}`);
-  expect(expected.getSchool()).toBe(test);
+  const expected = new Intern("Any", 123, test);
+  expect(expected.getSchool()).toBe(`Education: ${test}`);
 });
