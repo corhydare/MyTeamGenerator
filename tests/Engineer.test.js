36efb8478c89a42d2c0f74261e51c52ba9a4d500
Unknown <danila.vpopov@gmail.com>
2022-02-27 09:31:32 -0800

    Engineer makes a 2nd pass

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require("../lib/Engineer");
+
+test("Github username works with links and images", () => {
+  const testValue = "corhydare";
+  const e = new Engineer("Not Danila", 1, testValue);
+  expect(e.github).toBe(testValue);
+});
+
+test("getRole() will be Engineer instead of Employee", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Not Danila", 1, "corhydare");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("getGithub() works just as well", () => {
+  const testValue = "corhydare";
+  const e = new Engineer("Not Danila", 1, testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
